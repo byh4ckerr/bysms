8af9c9a39ea68468c12314b34734890765c93970
byh4ckerr <65884146+byh4ckerr@users.noreply.github.com>
2021-04-15 20:00:05 +0300

    Add files via upload

---
+++ tools/addons/logo.py
@@ -0,0 +1,12 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+_   ___       __   ____     __          
+  / _ )__ __/ /  / / /____/ /_____ ____
+ / _  / // / _ \/_  _/ __/  '_/ -_) __/
+/____/\_, /_//_/ /_/ \__/_/\_\\__/_/
+     /___/     
+>>>>> Coder By H4cker <<<<<<
+{Fore.RESET}""")
